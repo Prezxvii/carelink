11e12388d4ee9b8049a7b0fbc568d9a1f4ff5328
Timothy Brumell <122589070+Prezxvii@users.noreply.github.com>
2026-01-13 14:16:21 -0500

    Clean up resource route by removing unused code
    Removed commented-out code for NYC Open Data API call.

--- carelink-backend/routes/resources.js
+++ carelink-backend/routes/resources.js
@@ -5,9 +5,7 @@ const axios = require('axios');
 // Route to get resources (Food, Housing, etc.)
 router.get('/', async (req, res) => {
   try {
-    // You can replace this with a call to NYC Open Data Socrata API
-    // const response = await axios.get('https://data.cityofnewyork.us/resource/xxxx-xxxx.json');
-    // res.json(response.data);
+  
     
     res.json({ message: "Resource data endpoint connected" });
   } catch (err) {
